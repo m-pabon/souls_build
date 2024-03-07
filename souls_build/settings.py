51f034b1c8783525eb8f575074aa73d7e3e99bba
Michael Pabon <mpabon@outlook.com>
2024-03-07 12:04:34 -0500

    update postgres settings

--- souls_build/settings.py
+++ souls_build/settings.py
@@ -81,12 +81,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     },
     'postgres': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["DB_NAME"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': os.environ["DB_HOST"],
+        'PORT': os.environ["DB_PORT"],
     }
 }
 
