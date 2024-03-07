948d10a49490e4934313e947972163d0d26ed7ef
Michael Pabon <mpabon@outlook.com>
2024-03-07 16:31:40 -0500

    Added base url route and emoji favicon

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sould Build
+Django project used to create and share build from the FromSoft series of games. Starting with elden ring and starting
+with weapons.
+
+Current Live Site: https://soulsbuild-production.up.railway.app/
--- souls_build/urls.py
+++ souls_build/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import IndexView
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path("weapons/", include("weapons.urls")),
     path('admin/', admin.site.urls),
 ]
