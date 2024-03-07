f4423a5f9d4172daa5118c7a3acc60710b916b55
Michael Pabon <mpabon@outlook.com>
2024-03-07 12:18:35 -0500

    update start command

--- souls_build/settings.py
+++ souls_build/settings.py
@@ -77,16 +77,16 @@ WSGI_APPLICATION = 'souls_build.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'postgres': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ["DB_NAME"],
         'USER': os.environ["DB_USER"],
         'PASSWORD': os.environ["DB_PASSWORD"],
         'HOST': os.environ["DB_HOST"],
         'PORT': os.environ["DB_PORT"],
+    },
+    'local': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
