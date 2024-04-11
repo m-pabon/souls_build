5b7f7c04353c77424a225da6dfc9d17a4b56b338
Michael Pabon <mpabon@outlook.com>
2024-04-10 21:15:14 -0400

    Add games app

--- souls_build/urls.py
+++ souls_build/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from .views import IndexView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', include("games.urls")),
     path("weapons/", include("weapons.urls")),
     path('admin/', admin.site.urls),
 ]
--- souls_build/views.py
+++ souls_build/views.py
@@ -2,4 +2,4 @@ from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = 'souls_build/index.html'
+    template_name = 'souls_build/../games/templates/games/index.html'
